a36d20eb13437108d109c24c6d51036373ee2c05
Darja Ryazhskikh <darja.ryazhskikh@gmail.com>
2018-09-14 11:05:48 +0300

    One more test case for 4sum

--- test/FourSum.java
+++ test/FourSum.java
@@ -67,6 +67,17 @@ public class FourSum extends NestedListTestCase {
             {-2, 2, 3, 1}
         } );
 
+        testCase(new int[] {-3,-2,-1,0,0,1,2,3}, 0, new int[][] {
+            {-3,-2,2,3},
+            {-3,-1,1,3},
+            {-3,0,0,3},
+            {-3,0,1,2},
+            {-2,-1,0,3},
+            {-2,-1,1,2},
+            {-2,0,0,2},
+            {-1,0,0,1}
+        });
+
     }
 
     private void testCase(int[] nums, int target, int[][] expected) {
