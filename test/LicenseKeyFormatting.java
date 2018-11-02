a57f69089185b50402b7b4bf756afea1274f7640
Darja Ryazhskikh <darja.ryazhskikh@gmail.com>
2018-11-02 23:32:25 +0300

    License Key Formatting (https://leetcode.com/problems/license-key-formatting/)

---
+++ test/LicenseKeyFormatting.java
@@ -0,0 +1,36 @@
+import junit.framework.TestCase;
+
+@SuppressWarnings("WeakerAccess")
+public class LicenseKeyFormatting extends TestCase {
+    public String licenseKeyFormatting(String S, int K) {
+        StringBuilder sb = new StringBuilder();
+        int groupSize = 0;
+        for (int i = S.length() - 1; i >= 0; --i) {
+            char c = S.charAt(i);
+            if (c != '-') {
+                if (groupSize == K) {
+                    groupSize = 0;
+                    sb.insert(0, '-');
+                }
+                sb.insert(0, Character.toUpperCase(c));
+                groupSize++;
+            }
+
+        }
+
+        return sb.toString();
+    }
+
+    private void testOne(String input, int k, String expected) {
+        assertEquals(expected, licenseKeyFormatting(input, k));
+    }
+
+    public void test() {
+        testOne("a", 4, "A");
+        testOne("5F3Z-2e-9-w", 4, "5F3Z-2E9W");
+        testOne("2-5g-3-J", 2, "2-5G-3J");
+        testOne("abcc", 1, "A-B-C-C");
+        testOne("--abcc", 1, "A-B-C-C");
+        testOne("--a-a-a-a--", 2, "AA-AA");
+    }
+}
